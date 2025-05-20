bd491e76659d51522b7e7a1ab004b6453f51c41c
Amir <amir1330@localhost.localdomain>
2025-05-21 00:11:48 +0500

    ive tired

--- infrastructure/modules/lb/variables.tf
+++ infrastructure/modules/lb/variables.tf
@@ -1,3 +1,19 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "app_security_group_id" {
+  type = string
+}
+
+variable "instance_ids" {
+  type = list(string)
+}
+
 variable "project_name" {
   type = string
 } 
