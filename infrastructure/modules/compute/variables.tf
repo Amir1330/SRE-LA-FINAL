bfb48800c77f72efa67d03b4e22f2b45ef093242
Amir <amir1330@localhost.localdomain>
2025-05-21 00:09:18 +0500

    ive tired

--- infrastructure/modules/compute/variables.tf
+++ infrastructure/modules/compute/variables.tf
@@ -1,3 +1,15 @@
+variable "instance_count" {
+  type = number
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
 variable "project_name" {
   type = string
 } 
